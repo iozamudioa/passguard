55c0723317961e8d6835ea767eb3dcabdccdeb9a
Irving Oldayr <irving.oldayr.za@gmail.com>
2021-05-30 23:23:58 -0500

    username validation

--- src/main/java/net/iozamudioa/passguard/dto/UserDto.java
+++ src/main/java/net/iozamudioa/passguard/dto/UserDto.java
@@ -12,6 +12,8 @@ public class UserDto implements Serializable {
   private static final long serialVersionUID = 4752061078375667607L;
 
   private Integer idUser;
+
+  // @Size(min = 6, max = 6)
   private String username;
   private String password;
 
--- src/main/java/net/iozamudioa/passguard/util/UtilUsername.java
+++ src/main/java/net/iozamudioa/passguard/util/UtilUsername.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class UtilUsername {
 
-  private static final String REGEX_USERNAME = "^.\\S{6}$";
+  private static final String REGEX_USERNAME = "^(?=(?:.*[A-Z a-z]))\\S{6}$";
 
   public Boolean isValid(String username) {
     return username.matches(REGEX_USERNAME);
